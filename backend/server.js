cf33a2ceff58dc9782e6ff7a7546c13b46a00e3a
m-maximjuk <michael.maxim101@gmail.com>
2018-06-12 18:04:40 +0800

    update server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // set up ======================================================================
 var express = require('express');
-var app = express(); 						// create our app w/ express
+var app = express();				// create our app w/ express
+var cors = require('cors');
 var mongoose = require('mongoose'); 				// mongoose for mongodb
 var port = process.env.PORT || 8080; 				// set the port
 var database = require('./config/database'); 			// load the database config
@@ -10,8 +11,8 @@ var methodOverride = require('method-override');
 
 // configuration ===============================================================
 mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
-
 app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
+app.use(cors());
 app.use(morgan('dev')); // log every request to the console
 //app.setHeader("Access-Control-Allow-Origin", "*");
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
